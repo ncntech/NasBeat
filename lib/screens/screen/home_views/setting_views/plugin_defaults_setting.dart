5e85229a12757d9167db37602ef005b57d7a905d
nastech-ai <bantuinvasions@gmail.com>
2026-06-16 08:30:12 +0200

    brand: package:Bloomee/ → package:nasbeat/ [plugin_defaults_setting.dart]

--- lib/screens/screen/home_views/setting_views/plugin_defaults_setting.dart
+++ lib/screens/screen/home_views/setting_views/plugin_defaults_setting.dart
@@ -1,12 +1,12 @@
-import 'package:Bloomee/blocs/settings_cubit/cubit/settings_cubit.dart';
-import 'package:Bloomee/plugins/blocs/plugin/plugin_bloc.dart';
-import 'package:Bloomee/plugins/blocs/plugin/plugin_state.dart';
-import 'package:Bloomee/src/rust/api/plugin/plugin_info.dart';
-import 'package:Bloomee/screens/screen/home_views/setting_views/setting_shared_widgets.dart';
+import 'package:nasbeat/blocs/settings_cubit/cubit/settings_cubit.dart';
+import 'package:nasbeat/plugins/blocs/plugin/plugin_bloc.dart';
+import 'package:nasbeat/plugins/blocs/plugin/plugin_state.dart';
+import 'package:nasbeat/src/rust/api/plugin/plugin_info.dart';
+import 'package:nasbeat/screens/screen/home_views/setting_views/setting_shared_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:Bloomee/core/theme/app_theme.dart';
+import 'package:nasbeat/core/theme/app_theme.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:Bloomee/l10n/app_localizations.dart';
+import 'package:nasbeat/l10n/app_localizations.dart';
 import 'package:icons_plus/icons_plus.dart';
 
 class PluginDefaultsSettings extends StatelessWidget {
